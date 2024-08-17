84127f15b6191885d42189d4f37aedfe0d274a08
ungpyolee <uplee114@gmail.com>
2024-08-17 17:17:45 +0900

    Test : Deploy eb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import Image from 'next/image';
 export default function Home() {
     return (
         <>
-            <span className="font-en ">WEPLAN</span>
-            <p className="text-base">
-                반갑습니다. 일정을 관리하고 공유하는 서비스 <span className="font-bold">위플랜</span>입니다.
-            </p>
+            <div className="flex flex-col mt-10 text-center">
+                <span className="font-en text-2xl font-light">WEPLAN</span>
+                <p className="text-base text-lg">
+                    반갑습니다. 일정을 관리하고 공유하는 서비스 <span className="font-semibold">위플랜</span>입니다.
+                </p>
+            </div>
         </>
     );
 }
