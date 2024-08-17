e2830bb4ce9e76f01e1ea3f74eb52e14669a2136
ungpyolee <uplee114@gmail.com>
2024-08-17 17:08:07 +0900

    Feat : Add AWS_ACCOUNT_ID env in github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import Image from 'next/image';
 export default function Home() {
     return (
         <>
-            <p className="text-base">반갑습니다</p>
             <span className="font-en ">WEPLAN</span>
+            <p className="text-base">
+                반갑습니다. 일정을 관리하고 공유하는 서비스 <span className="font-bold">위플랜</span>입니다.
+            </p>
         </>
     );
 }
