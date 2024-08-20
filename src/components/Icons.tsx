0a7b9ffd5baf5bfe83cdbce9a5bda71ac416f4a2
uplee <164474385+ungpyolee@users.noreply.github.com>
2024-08-20 20:53:46 +0900

    Feat : BottomNavbar, Input, LoginForm 컴포넌트 추가 (#5)
    - Input, BottomNavbar 컴포넌트 추가
    - LoginForm 개발 중
    Related to:#1, #3

---
+++ src/components/Icons.tsx
@@ -0,0 +1,35 @@
+import LogoWeplan from '../assets/icons/logo-weplan.svg';
+import IconLikeL from '../assets/icons/icon-like-l.svg';
+import IconLike from '../assets/icons/icon-like.svg';
+import IconAddL from '../assets/icons/icon-add-l.svg';
+import IconAddLB from '../assets/icons/icon-add-l-b.svg';
+import IconBookmark from '../assets/icons/icon-bookmark.svg';
+import IconBookmarkL from '../assets/icons/icon-bookmark-l.svg';
+import IconBookmarkLB from '../assets/icons/icon-bookmark-l-b.svg';
+import IconFilterL from '../assets/icons/icon-filter-l.svg';
+import IconHomeL from '../assets/icons/icon-home-l.svg';
+import IconHomeLB from '../assets/icons/icon-home-l-b.svg';
+import IconMypageL from '../assets/icons/icon-mypage-l.svg';
+import IconMypageLB from '../assets/icons/icon-mypage-l-b.svg';
+import IconSearchL from '../assets/icons/icon-search-l.svg';
+import IconSearchLB from '../assets/icons/icon-search-l-b.svg';
+import IconsettingL from '../assets/icons/icon-setting-l.svg';
+
+export {
+    LogoWeplan,
+    IconAddL,
+    IconAddLB,
+    IconBookmark,
+    IconBookmarkL,
+    IconBookmarkLB,
+    IconFilterL,
+    IconHomeL,
+    IconHomeLB,
+    IconLike,
+    IconLikeL,
+    IconMypageL,
+    IconMypageLB,
+    IconSearchL,
+    IconSearchLB,
+    IconsettingL,
+};
