d0724fab10756dc97b245068e166ee7d00c2b17b
ungpyolee <uplee114@gmail.com>
2024-08-20 20:58:37 +0900

    Feat : svg파일 컴포넌트로 모듈화, 페이지라우팅
    - svgr/webpack 라이브러리 설치
    - svg파일 모듈화 Icons.tsx 컴포넌트 추가
    - BottomNavBar에 필요한 페이지 구조
    
    Realted to: #1, #2, #3, #6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { LogoWeplan } from '@/components/Icons';
+
 export default function Home() {
     return (
         <>
-            <div className="flex flex-col mt-10 text-center">
-                <span className="font-en text-2xl font-light">WEPLAN</span>
+            <div className="flex flex-col text-center">
+                <LogoWeplan height="24" className="fill-primary mt-10 mb-2" />
                 <p className="text-lg">
                     반갑습니다. 일정을 관리하고 공유하는 서비스 <span className="font-semibold">위플랜</span>입니다.
                 </p>
