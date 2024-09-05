38c5a2fe8cee6505affd5d97b11abeca7a6f0740
ungpyolee <uplee114@gmail.com>
2024-09-06 00:15:40 +0900

    [#1] Feat : InputFileProfile 컴포넌트 추가
    - 프로필 사진업데이트를 위한 컴포넌트 추가

--- src/app/create-plan/complete/page.tsx
+++ src/app/create-plan/complete/page.tsx
@@ -20,13 +20,13 @@ const page = () => {
             <section className="pt-10 flex flex-col">
                 <InputText title="제목과 설명" placeholder="작성 일정에 대한 제목이나 설명을 입력해주세요" />
                 <div className="my-8 px-5">
-                    <p className="ps-1 mb-1">여행 지역</p>
+                    <p className="ps-1 mb-1">대표 지역</p>
                     <div className="flex items-center">
                         <div className="flex items-center gap-1 text-gray-300 me-auto ps-1">
                             <div className="w-3 h-3 flex justify-center items-center">
                                 <IconPin className="h-full fill-gray-400" />
                             </div>
-                            <p className="line-clamp-1">이번 일정의 대표 지역을 설정해주세요</p>
+                            <p className="line-clamp-1">이번 일정의 대표지역을 설정</p>
                         </div>
                         <ButtonDefault value="장소 검색" size="sm" />
                     </div>
---
+++ src/components/common/Input/InputFileImages.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const InputFileImages = () => {
+    return <div>InputFileImages</div>;
+};
+
+export default InputFileImages;
